925f72f2d3a4e8f055da91d1badbb609aaf667ec
bbhargava <bh375119@dal.ca>
2020-04-03 16:35:10 -0300

    comments

--- app/src/main/java/com/CSCI5708/dalbike/loginview.kt
+++ app/src/main/java/com/CSCI5708/dalbike/loginview.kt
@@ -21,7 +21,7 @@ import java.util.*
 class loginview : AppCompatActivity() {
 
     lateinit var ref: DatabaseReference
-    lateinit var bannerId : EditText
+    lateinit var bannerId: EditText
     lateinit var dpd: DatePickerDialog
     lateinit var dateText: TextView
     private val sharedPrefFile = "kotlinsharedpreference"
@@ -37,21 +37,21 @@ class loginview : AppCompatActivity() {
 
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        // to set bottom navigation bar
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
 
-            when(item.itemId){
+            when (item.itemId) {
 
                 R.id.nav_bikes -> {
-                    val intent = Intent(this,HomeActivity::class.java)
+                    val intent = Intent(this, HomeActivity::class.java)
                     startActivity(intent)
                 }
 
                 R.id.nav_profile -> {
-                    if(LoggedInUserModel.loggedInUser.equals("")){
-                        val intent = Intent(this,loginview::class.java)
+                    if (LoggedInUserModel.loggedInUser.equals("")) {
+                        val intent = Intent(this, loginview::class.java)
                         startActivity(intent)
-                    }
-                    else {
+                    } else {
                         val intent = Intent(this, MyProfile::class.java)
                         startActivity(intent)
                     }
@@ -68,58 +68,71 @@ class loginview : AppCompatActivity() {
 
     }
 
-    fun fundate (view:View){
+    // to get date of birth from date picker
+    fun fundate(view: View) {
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-        dpd = DatePickerDialog(this, android.R.style.Theme_Holo_Dialog, DatePickerDialog.OnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
-            date1.text ="$dayOfMonth/$monthOfYear/$year"
-        }, year, month, day)
+        dpd = DatePickerDialog(
+            this,
+            android.R.style.Theme_Holo_Dialog,
+            DatePickerDialog.OnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
+                date1.text = "$dayOfMonth/$monthOfYear/$year"
+            },
+            year,
+            month,
+            day
+        )
         dpd.show()
     }
 
 
-    fun checkCred(view: View){
-        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,Context.MODE_PRIVATE)
-        var enteredBanner:String = bannerId.text.toString()
+    // to check user login credentials from database
+    fun checkCred(view: View) {
+        val sharedPreferences: SharedPreferences =
+            this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        var enteredBanner: String = bannerId.text.toString()
         var enteredDOB = date1.text
         var flag = -1
-        var bannerId:String = ""
-        var Dob:String = ""
+        var bannerId: String = ""
+        var Dob: String = ""
 
         //Check for user
         ref.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError?) {
 
             }
-            override fun onDataChange(p0: DataSnapshot?) {
-                val editor:SharedPreferences.Editor =  sharedPreferences.edit()
 
-                if(p0!!.exists()){
-                    for(p in p0.children){
+            override fun onDataChange(p0: DataSnapshot?) {
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+                if (p0!!.exists()) {
+                    for (p in p0.children) {
                         val user = p.getValue(Users::class.java)
-                        if(user!!.bannerId.equals(enteredBanner) && user!!.DOB.equals(enteredDOB)){
-                            System.out.println("done")
+                        if (user!!.bannerId.equals(enteredBanner) && user!!.DOB.equals(enteredDOB)) {
                             flag = 1
-                            editor.putBoolean("is_user_logged_in",true)
+                            editor.putBoolean("is_user_logged_in", true)
                             editor.apply()
                             editor.commit()
                             LoggedInUserModel.loggedInUser = "yes"
                             loginSucessfullLoginUser()
                             break
-                        }else{
-                            System.out.println("error")
-                            editor.putBoolean("is_user_logged_in",false)
+                        } else {
+                            editor.putBoolean("is_user_logged_in", false)
                             editor.apply()
                             editor.commit()
-                            val toast = Toast.makeText(applicationContext, "Wrong Banner ID or Date of Birth", Toast.LENGTH_SHORT)
+                            val toast = Toast.makeText(
+                                applicationContext,
+                                "Wrong Banner ID or Date of Birth",
+                                Toast.LENGTH_SHORT
+                            )
                             toast.show()
                         }
                     }
                 }
-                if (flag==1){
+                if (flag == 1) {
                     intent = Intent(applicationContext, HomeActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(intent)
@@ -131,11 +144,11 @@ class loginview : AppCompatActivity() {
     }
 
 
-    fun loginSucessfullLoginUser(){
+    // shows user profile if login successful
+    fun loginSucessfullLoginUser() {
         val intent = Intent(this, MyProfile::class.java)
         startActivity(intent)
     }
-
 
 
 }
